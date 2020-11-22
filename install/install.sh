6f61acddfd85c6d7c552b1271be0b42d94bc62bf
TamaTamaGoGo <tamatama@tensi.org>
2020-11-22 10:06:00 +0900

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,3 +4,4 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager                                                               
 sudo apt update                                                                                                     
 sudo apt install -y yad                                                                                             
+dir=$(pwd)
